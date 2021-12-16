2649ae859c6b57ba703059480d43861a7bdd1bd0
adciechomski <adciechomski@poczta.onet.pl>
2021-12-16 14:56:03 -0700

    http update

--- app/factory.py
+++ app/factory.py
@@ -4,7 +4,7 @@ def create_file_grabber():
     path = get_resource_path()
     if "http" in path:
         print("Program is not getting files on internet yet, to get local file.")
-        return    
+        exit()    
 
     file_grabber = GetFileFromPath(resource_location=path)
     return file_grabber
